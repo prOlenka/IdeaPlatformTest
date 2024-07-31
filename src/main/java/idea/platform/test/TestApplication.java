55695c7a4e270b9e9b0636801f908fc7e7e51ba7
olia.prosha@gmail.com <olia.prosha@gmail.com>
2024-07-31 11:31:19 +0300

    first

---
+++ src/main/java/idea/platform/test/TestApplication.java
@@ -0,0 +1,11 @@
+package idea.platform.test;
+
+import org.springframework.boot.SpringApplication;
+
+public class TestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestApplication.class, args);
+	}
+
+}
